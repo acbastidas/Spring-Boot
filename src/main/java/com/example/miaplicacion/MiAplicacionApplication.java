2b2dd8f3908c06a4ce35bb3bc39ca2c0a7f82944
Alvaro Contreras Bastidas <ac91.bastidas@gmail.com>
2025-03-30 18:10:41 -0500

    Correccion de codigo para ejecucuion

--- src/main/java/com/example/miaplicacion/MiAplicacionApplication.java
+++ src/main/java/com/example/miaplicacion/MiAplicacionApplication.java
@@ -2,10 +2,8 @@ package com.example.miaplicacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
-@EnableReactiveMongoRepositories(basePackages = "com.example.miaplicacion.repository")
 public class MiAplicacionApplication {
 
     public static void main(String[] args) {
