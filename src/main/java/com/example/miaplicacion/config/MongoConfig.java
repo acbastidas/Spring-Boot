dbaee6d230cee62da39375b6d6b5fd6518df50d1
Alvaro Contreras Bastidas <ac91.bastidas@gmail.com>
2025-03-29 19:01:08 -0500

    Cambios en configuracion de mongo y en pom

---
+++ src/main/java/com/example/miaplicacion/config/MongoConfig.java
@@ -0,0 +1,15 @@
+package com.example.miaplicacion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public ReactiveMongoTemplate reactiveMongoTemplate(ReactiveMongoDatabaseFactory factory) {
+        return new ReactiveMongoTemplate(factory);
+    }
+}
